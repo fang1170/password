8bd803010ac3d77ed29a3fb023be6ccb559ab87a
Jonny Fang <b00130117@gmail.com>
2023-02-06 16:45:11 +0800

    last

--- pwd.py
+++ pwd.py
@@ -1,10 +1,12 @@
 password = '123'
 i = 3
 while i > 0:
+	i=i-1
 	swd = input('請輸入密碼')
 	if swd == password:
 		print('登入成功')
 		break
 	else:
-	 	i=i-1
-	 	print('密碼錯誤剩餘',i,'次機會')
+		print('密碼錯誤剩餘')
+		if i > 0:
+	 		print('剩餘', i, '次機會')
